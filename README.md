0ead435ba64192d82569a366cdf838c63601e97b
Hector <hector.pascual@silicon-gears.com>
2019-07-31 13:46:51 +0200

    [1.0] Added readme, added pipfile, added source code and demo map html

---
+++ README.md
@@ -0,0 +1,30 @@
+# ETSETB - Enterprises map
+
+This repository contains a python script that generates a map using Google Maps API with a marker per
+each enterprise which the ETSETB has an agreement, so it is easier for students to filter for enterprises
+that may interest them depending on its location.
+
+## Developing
+
+First install pipenv :
+
+`$ pip install pipenv`
+
+In order to develop or run the script you must run pipenv to install the required dependencies.
+
+`$ pipenv install`
+
+You must have defined the following environment vars in order to run the script : 
+
+|Name|Description|
+|-------|------|
+|UPC_USER|Username for the UPC intranet login|
+|UPC_PASS|Password for the UPC intranet login|
+|MAPS_API_KEY|API Key for google maps (contact me if you don't own a key)|
+
+### Running the script :
+
+~~~
+$ pipenv shell
+$ python main.py
+~~~
---
+++ src/main.py
@@ -0,0 +1,58 @@
+import googlemaps
+import gmplot
+import re
+import os
+import requests
+
+gmaps = googlemaps.Client(key=os.environ['MAPS_API_KEY'])
+
+
+def get_enterprises():
+    """
+    Logs to intranet with a POST method submitting the form fields and then makes a GET method,
+    and searchs for a pattern in order to get a list of the enterprises
+
+    :return:The list containing the enterprises
+    """
+    url = 'https://intranet.etsetb.upc.edu/serveis/estudiants/relacions_externes/llistat_empreses.html'
+    url_login = 'https://intranet.etsetb.upc.edu/sys/login.html'
+    payload = {'login': os.environ['UPC_USER'],'password': os.environ['UPC_PASS'],'submit':'inici de Sessió'}
+
+    with requests.Session() as s:
+        s.post(url_login, data=payload)
+        res = s.get(url)
+        data = re.findall('<tr><.*">(.*)</td', res.text)
+        return data
+
+
+def parse_name_to_marker(enterprises):
+    """
+    Uses google maps API in order to obtain get the closest address to Barcelona from each
+    enterprise office in the list it also gets its website and displays the info in the map with a
+    marker.
+    :param enterprises:  the list of enterprises that we want to allocate in the map
+    """
+    gmap = gmplot.GoogleMapPlotter(41.39021337, 2.1357438,13, os.environ['MAPS_API_KEY'])
+    addr = []
+    for enterp in enterprises:
+        print("Parsing", enterp)
+        addr_tmp = gmaps.places(query=enterp, location=(41.39021337, 2.1357438))
+        if addr_tmp['status'] == 'OK':
+            addr.append(addr_tmp['results'][0]['formatted_address'])
+            website = gmaps.place(addr_tmp['results'][0]['place_id'], fields=['website'])['result']
+            if bool(website):
+                gmap.marker(addr_tmp['results'][0]['geometry']['location']['lat'],
+                            addr_tmp['results'][0]['geometry']['location']['lng'],
+                            title=("<b><u>" + enterp +"</u></b>"), label=("<br /> <b>Website :</b> "
+                                   + "<a href=" + website['website']+">"+website['website']+"</a>"))
+            else:
+                gmap.marker(addr_tmp['results'][0]['geometry']['location']['lat'],
+                            addr_tmp['results'][0]['geometry']['location']['lng'],
+                            title=("<b><u>" + enterp + "</u></b>"), label="<br/> No website found")
+    print("Ended parsing and querying")
+    gmap.draw('../map/map.html')
+
+
+if __name__ == "__main__":
+    enterprises = get_enterprises()
+    parse_name_to_marker(enterprises)
